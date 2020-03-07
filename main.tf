cb5dee75adfeb5f73234677d0a2b3b017c972f9c
Daiki Horiyama <d19921207@gmail.com>
2020-03-07 16:33:28 +0900

    added natgateway with eip, route

--- main.tf
+++ main.tf
@@ -69,3 +69,21 @@ resource "aws_route_table_association" "private" {
   subnet_id = aws_subnet.private.id
   route_table_id = aws_route_table.private.id
 }
+
+
+resource "aws_eip" "nat_gateway" {
+  vpc = true
+  depends_on = [aws_internet_gateway.example]
+}
+
+resource "aws_nat_gateway" "example" {
+  allocation_id = aws_eip.nat_gateway.id
+  subnet_id = aws_subnet.public.id
+  depends_on = [aws_internet_gateway.example]
+}
+
+resource "aws_route" "private" {
+  route_table_id = aws_route_table.private.id
+  nat_gateway_id = aws_nat_gateway.example.id
+  destination_cidr_block = "0.0.0.0/0"
+}
