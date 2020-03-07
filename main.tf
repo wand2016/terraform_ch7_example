216af44f133785fde4e58e8347b10dfaa3a7b9a4
Daiki Horiyama <d19921207@gmail.com>
2020-03-07 15:56:33 +0900

    add ルート

--- main.tf
+++ main.tf
@@ -34,3 +34,9 @@ resource "aws_route_table" "public" {
     Name = "example"
   }
 }
+
+resource "aws_route" "public" {
+  route_table_id = aws_route_table.public.id
+  gateway_id = aws_internet_gateway.example.id
+  destination_cidr_block = "0.0.0.0/0"
+}
