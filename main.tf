e2839fce751f8d8f132bc30e14a1b5d6e6a96ea7
Daiki Horiyama <d19921207@gmail.com>
2020-03-07 16:11:35 +0900

    プライベートサブネット用のroute, route association追加

--- main.tf
+++ main.tf
@@ -56,3 +56,16 @@ resource "aws_subnet" "private" {
     Name = "example_private"
   }
 }
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.example.id
+
+  tags = {
+    Name = "example_private"
+  }
+}
+
+resource "aws_route_table_association" "private" {
+  subnet_id = aws_subnet.private.id
+  route_table_id = aws_route_table.private.id
+}
